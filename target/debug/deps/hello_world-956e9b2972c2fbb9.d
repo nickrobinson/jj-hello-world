16adfe1d50b92c8e1f0be69b2a740cb5e30d813a
Nick Robinson <nick.robinson@ditto.com>
2026-03-16 13:33:58 -0400

    hello world

---
+++ target/debug/deps/hello_world-956e9b2972c2fbb9.d
@@ -0,0 +1,5 @@
+/Users/nickrobinson/Development/sandbox/hello-world/target/debug/deps/hello_world-956e9b2972c2fbb9.d: src/main.rs
+
+/Users/nickrobinson/Development/sandbox/hello-world/target/debug/deps/hello_world-956e9b2972c2fbb9: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/nickrobinson/Development/sandbox/hello-world/target/debug/hello-world: /Users/nickrobinson/Development/sandbox/hello-world/src/main.rs
